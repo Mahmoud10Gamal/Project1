a5002e76bf4972ff151357d87d3dc851f1724ab7
Mahmoud10Gamal <ma7moudismail47@gmail.com>
2024-11-10 19:36:02 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿namespace Array
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int Smallcarpets, largecarpets;
+            Console.WriteLine("Please enter your number of small carpets");
+            Smallcarpets = int.Parse(Console.ReadLine());
+            Console.WriteLine("Please enter your number of large carpets");
+            largecarpets = int.Parse(Console.ReadLine());
+            Console.WriteLine("price per small carpets : 25$");
+            Console.WriteLine("price per large carpets : 35$");
+            Console.WriteLine("The cost: 110$ But there is a tax is 6.6$ ");
+            Console.WriteLine("The Total estimate is 116.6$");
+            Console.WriteLine("This estimate is valid for 30 days");
+
+
+        }
+    }
+}
